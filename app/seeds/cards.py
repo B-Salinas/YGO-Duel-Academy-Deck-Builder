81039922ad6f9bd222ced82031be6a0fd1eb74ef
B Salinas <b.salinas397@gmail.com>
2021-06-08 13:19:51 -0500

    added yugioh gx duel academy json file w all the cards from the game

--- app/seeds/cards.py
+++ app/seeds/cards.py
@@ -1,9 +1,17 @@
-from app.models import db, Monster_Card, Spell_Trap_Card
+# from app.models import db, Monster_Card, Spell_Trap_Card
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, unquote
 from json import loads
 
+# KEEP THESE
+useragent = "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion"
+headers = {
+    "User-Agent": useragent
+}
 
 def whatever():
-    all_cards_data = urlopen("https://db.ygoprodeck.com/api/v7/cardinfo.php").read().decode("UTF-8")
-    
+    all_cards_data = urlopen(Request("https://db.ygoprodeck.com/api/v7/cardinfo.php", headers=headers)).read().decode("UTF-8")
+    print(loads(all_cards_data))
+
+
+whatever()
