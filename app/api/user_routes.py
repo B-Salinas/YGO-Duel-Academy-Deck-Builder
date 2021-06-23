089d2009abe81f246c9af8ffca477ea75ec390f7
B Salinas <b.salinas397@gmail.com>
2021-06-22 21:50:50 -0500

    thinking about decks

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -5,14 +5,27 @@ from app.models import User
 user_routes = Blueprint('users', __name__)
 
 @user_routes.route('/')
-@login_required
+# @login_required
 def users():
     users = User.query.all()
     return {"users": [user.to_dict() for user in users]}
 
-
 @user_routes.route('/<int:id>')
-@login_required
-def user(id):
+# @login_required
+def user(ids):
     user = User.query.get(id)
+    return user.to_dict()
+
+
+@user_routes.route('/decks/')
+# @login_required
+def decks():
+    # user = User.query.get(id) 
+    return user.to_dict()
+
+
+@user_routes.route('/decks/<int:id>')
+# @login_required
+def deck(deck_id):
+    deck = User.query.get(deck_id)
     return user.to_dict()
