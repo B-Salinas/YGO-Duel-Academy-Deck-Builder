435907ead7cd08006506e1f713d852d1cac9ac36
B Salinas <b.salinas397@gmail.com>
2021-07-10 16:18:18 -0500

    WE CAN ADD A NEW DECK

--- app/api/deck_routes.py
+++ app/api/deck_routes.py
@@ -1,8 +1,26 @@
+from flask import Blueprint, jsonify, request
 from app.models import db, Deck
-from flask import Blueprint, jsonify
+from flask_login import current_user
+from app.forms import NewDeckForm
 
 deck_routes = Blueprint("decks", __name__)
 #  /api/decks...
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f"{field}: {error}")
+    return errorMessages
+
+
+
+
+
+
 
 # D E C K S
 @deck_routes.route('/')
@@ -11,10 +29,24 @@ def get_all_decks():
     return jsonify([deck.to_dict() for deck in decks])
 
 @deck_routes.route('/', methods=["POST"])
-def add_deck():
-    # I DONT EVEN KNOW HOW TO BEGIN THIS ROUTE
-    decks = Deck.query.all()
-    return jsonify([deck.to_dict() for deck in decks])
+def add_new_deck():
+    form = NewDeckForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    
+    if form.validate_on_submit():
+        data = form.data
+        new_deck = Deck(
+            name = data['name'],
+            user_id = data['user_id']
+            
+        )
+        
+        db.session.add(new_deck)
+        db.session.commit()
+
+        return new_deck.to_dict()
+    
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 @deck_routes.route('/<int:id>')
 def get_one_deck(id):
@@ -29,4 +61,7 @@ def delete_deck(id):
     return {"success": "success"}
 
 # D E C K   C A R D S 
-
+@deck_routes.route('/<int:deck_id>/all')
+def get_all_cards_in_deck(deck_id):
+    deck = Deck.query.get(deck_id)
+    return jsonify([card.to_dict() for card in deck.cards])
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,4 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+
+from .deck_form import NewDeckForm, AddToDeck, RemoveFromDeck
