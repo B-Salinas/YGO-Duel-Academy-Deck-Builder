a7a901186fd264ec20f5f32301be3f890765cb1d
B Salinas <b.salinas397@gmail.com>
2022-03-28 07:16:23 -0500

    formatting so i can get your changes

--- react-app/src/components/Buttons/EditDeckButton.js
+++ react-app/src/components/Buttons/EditDeckButton.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { IconButton } from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
+
 function EditDeckButton() {
-  return <></>;
+  return (
+    <>
+      <IconButton colorScheme="gray" icon={<EditIcon />} /> 
+    </>
+  );
 }
 
 export default EditDeckButton;
